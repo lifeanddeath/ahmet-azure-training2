3829e53e99c180209d693299db33d9fd43ee0b8d
Ahmet Eroglu <aeroglu2016@gmail.com>
2022-05-06 15:11:57 +0300

    add basic routing to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}} v2.1</div>`,
+  template: `<h1>Angular Router App</h1>
+  <!-- This nav gives you links to click, which tells the router which route to use (defined in the routes constant in  AppRoutingModule) -->
+  <nav>
+    <ul>
+      <li><a routerLink="/users" routerLinkActive="active">Users</a></li>
+      <li><a routerLink="/products" routerLinkActive="active">Products</a></li>
+    </ul>
+  </nav>
+  <!-- The routed views render in the <router-outlet>-->
+  <router-outlet></router-outlet>`,
 })
 export class AppComponent {
   value = 'World';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { UsersComponent } from './users/users.component';
+import { ProductsComponent } from './products/products.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [{path: 'users',component: UsersComponent},{path:'products', component: ProductsComponent}]
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, UsersComponent, ProductsComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
